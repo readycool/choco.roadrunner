342fd1993680d717f115e53f7c22f6d41800ac4a
Ante Dražić <96959476+readycool@users.noreply.github.com>
2026-04-18 11:32:37 +0200

    v2025.1.13

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 $packageArgs = @{
   packageName    = "$env:ChocolateyPackageName"
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-  url64bit       = 'https://github.com/roadrunner-server/roadrunner/releases/download/v2025.1.12/roadrunner-2025.1.12-windows-amd64.zip'
-  checksum64     = '491244dc00ec58a4996bd0434eea6b4d877ea791e7ea6b75e294b9c04d9b2425196ae17884e61359dc793089d3f638dcaee8e3ffd1f9cfacc57cacefa855fda5'
+  url64bit       = 'https://github.com/roadrunner-server/roadrunner/releases/download/v2025.1.13/roadrunner-2025.1.13-windows-amd64.zip'
+  checksum64     = '677c0d985701e77c8cace6ec06d7e096c35f2c56196383aadc5dfae50c6bfe2923937211a74b81a71273e44bdf0bb9aeece5112e3e6b525152a8048525d3019c'
   checksumType64 = 'sha512'
 }
 
